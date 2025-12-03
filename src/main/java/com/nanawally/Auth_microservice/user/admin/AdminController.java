847281d7b06c75c5c90e0b9a5bf3e4f349ce15d1
liluppis <lisa.upp@hotmail.com>
2025-12-03 15:06:01 +0100

    remove: unsued attributes and imports in controller

--- src/main/java/com/nanawally/Auth_microservice/user/admin/AdminController.java
+++ src/main/java/com/nanawally/Auth_microservice/user/admin/AdminController.java
@@ -1,18 +1,11 @@
 package com.nanawally.Auth_microservice.user.admin;
 
 import com.nanawally.Auth_microservice.user.CustomUser;
-import com.nanawally.Auth_microservice.user.CustomUserRepository;
-import com.nanawally.Auth_microservice.user.authority.UserRole;
 import com.nanawally.Auth_microservice.user.dto.CustomUserCreationDTO;
-import com.nanawally.Auth_microservice.user.dto.CustomUserSelfRegisterDTO;
-import com.nanawally.Auth_microservice.user.mapper.CustomUserMapper;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -21,22 +14,15 @@ import java.util.*;
 @RequestMapping("/admin")
 public class AdminController {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private final CustomUserRepository customUserRepository;
-    private final CustomUserMapper customUserMapper;
-    private final PasswordEncoder passwordEncoder;
     private final AdminService adminService;
 
     @Autowired
-    public AdminController(CustomUserRepository customUserRepository, CustomUserMapper customUserMapper, PasswordEncoder passwordEncoder, AdminService adminService) {
-        this.customUserRepository = customUserRepository;
-        this.customUserMapper = customUserMapper;
-        this.passwordEncoder = passwordEncoder;
+    public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
 
     @GetMapping("/users")
-    public ResponseEntity<List<CustomUser>> getAllUsers(){
+    public ResponseEntity<List<CustomUser>> getAllUsers() {
 
         if (adminService.getAllUsers().isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -47,8 +33,8 @@ public class AdminController {
 
     @PostMapping("/register")
     public ResponseEntity<CustomUser> registerUser(@Valid @RequestBody CustomUserCreationDTO dto) {
-         CustomUser newUser = adminService.registerUser(dto);
-         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        CustomUser newUser = adminService.registerUser(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
     @DeleteMapping("/delete/{id}")
